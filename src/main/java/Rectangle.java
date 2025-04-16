b6ae4aa00ab7dbc53a8a9c45fb45e4e47c75cc85
jonathan-zhao-ics4u1-03-25 <jonathan.zhao@student.tdsb.on.ca>
2025-04-16 19:03:10 +0000

    Done

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,33 @@
+public class Rectangle {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength() {
+        return this.length;
+    }
+
+    public double getWidth() {
+        return this.width;
+    }
+
+    public double area() {
+		if (this.length == 0 || this.width == 0) {
+			return 0;
+		} else {
+        return (this.length * this.width);
+    }
+}
+
+    public double perimeter() {
+		if (this.length == 0 || this.width == 0) {
+        return 0;
+    } else {
+		return 2 * (this.length + this.width);
+	}
+	} 
+}
